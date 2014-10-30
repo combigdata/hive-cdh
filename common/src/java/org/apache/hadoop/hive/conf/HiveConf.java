ac1b570eb6daf9aefc05c029c2f3804d617d65f3
Gunther Hagleitner <gunther@apache.org>
2014-10-30 18:04:34 +0000

    HIVE-8660: sql std auth: property missing from whitelist - hive.exec.dynamic.partition.mode (Thejas M Nair via Gunther Hagleitner)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1635568 13f79535-47bb-0310-9956-ffa450edef68
    (cherry picked from commit ab651038007272ef8332eae2c59c199a23306362)

--- common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
+++ common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
@@ -2589,6 +2589,7 @@ public class HiveConf extends Configuration {
     "hive\\.auto\\..*",
     "hive\\.cbo\\..*",
     "hive\\.convert\\..*",
+    "hive\\.exec\\.dynamic\\.partition.*",
     "hive\\.exec\\..*\\.dynamic\\.partitions\\..*",
     "hive\\.exec\\.compress\\..*",
     "hive\\.exec\\.infer\\..*",
